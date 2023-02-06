7f3b0f888986a12e752b849aca1a3a1db00f3e98
Shubhamraut9497 <114891419+Shubhamraut9497@users.noreply.github.com>
2023-02-06 19:18:22 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,22 +1,42 @@
-import React ,{useRef,useState,useEffect} from 'react'
-import '../styles/App.css';
-import InputField from './InputField.js';
+import React, { useRef, useState, useEffect } from "react";
+import "../styles/App.css";
+import InputField from "./InputField.js";
 const App = () => {
- 
-//code here 
+  //code here
+  const [value, setValue] = useState("");
+  const [values, setV2] = useState("");
+  // const v = () => {
+  //   setValue(value);
+  // };
 
+  // const newRef = React.createRef();
 
- 
+  const newRef = useRef(null);
+
+  const settingValue = () => {
+    setV2(value);
+    setValue("");
+  };
+  const focusInput = (event) => {
+    newRef.current.focus();
+  };
+
   return (
     <div>
-    <InputField  ref={newRef}  type="text"/><br/>
-    <button id="settingValueButton" onClick={settingValue}>Set Value</button>
-    <button id="focusInputButton" onClick={focusInput}>Focus the input</button><br/><br/>
-     <textarea id="textarea" value={values}></textarea>
-
+      <InputField ref={newRef} type="text" value={value} setValue={setValue} />
+      <br />
+      <button id="settingValueButton" onClick={settingValue}>
+        Set Value
+      </button>
+      <button id="focusInputButton" onClick={focusInput}>
+        Focus the input
+      </button>
+      <br />
+      <br />
+      <textarea id="textarea" value={values}></textarea>
     </div>
   );
-}
-
+};
 
 export default App;
+
